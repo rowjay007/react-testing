2b8a6e3cf386b49c9b2059132686d542fd7281d4
Rowland Adimoha <rowlives@gmail.com>
2021-01-20 08:50:46 +0100

    refactor appTest with beforeEach function

--- src/components/__test__/App.test.js
+++ src/components/__test__/App.test.js
@@ -4,11 +4,15 @@ import App from "../App";
 import CommentBox from "../CommentBox";
 import CommentList from "../CommentList";
 
+let wrapped;
+
+beforeEach(() => {
+  wrapped = shallow(<App />);
+});
+
 it("shows a comment box", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentBox).length).toEqual(1);
 });
 it("shows a comments list", () => {
-  const wrapped = shallow(<App />);
   expect(wrapped.find(CommentList).length).toEqual(1);
 });
