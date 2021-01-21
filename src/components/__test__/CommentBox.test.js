f8efc9ecb3b036d2b85dd0233d7a9a9da3e675ef
Rowland Adimoha <rowlives@gmail.com>
2021-01-21 08:41:57 +0100

    successful test with 'mount' function on CommentBox component

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -1,4 +1,13 @@
 import React from "react";
-import CommentBox from "../../components/CommentBox";
+import { mount } from "enzyme";
+import CommentBox from "../CommentBox";
 
-it("has a text area and a button", () => {});
+let wrapped;
+beforeEach(() => {
+  wrapped = mount(<CommentBox />);
+});
+
+it("has a text area and a button", () => {
+  expect(wrapped.find("textarea").length).toEqual(1);
+  expect(wrapped.find("button").length).toEqual(1);
+});
