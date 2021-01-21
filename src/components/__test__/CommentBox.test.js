6717bcfd691804fbcc165b2685d4c51368c9eac3
Rowland Adimoha <rowlives@gmail.com>
2021-01-21 09:46:27 +0100

    refactor with describe() function

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -14,20 +14,24 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
-it("has a text area that users can type in", () => {
-  wrapped
-    .find("textarea")
-    .simulate("change", { target: { value: "new comment" } });
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
-});
+describe("the text area", () => {
+  beforeEach(() => {
+    wrapped
+      .find("textarea")
+      .simulate("change", { target: { value: "new comment" } });
+    wrapped.update();
+  });
+  it("has a text area that users can type in", () => {
+    expect(wrapped.find("textarea").prop("value")).toEqual("new comment");
+  });
 
-it("when form is submitted, text area gets emptied", () => {
-  wrapped
-    .find("button")
-    .simulate("change", { target: { value: "new comment" } });
-  wrapped.update();
-  wrapped.find("form").simulate("submit");
-  wrapped.update();
-  expect(wrapped.find("textarea").prop("value")).toEqual("");
+  it("when form is submitted, text area gets emptied", () => {
+    wrapped
+      .find("button")
+      .simulate("change", { target: { value: "new comment" } });
+    wrapped.update();
+    wrapped.find("form").simulate("submit");
+    wrapped.update();
+    expect(wrapped.find("textarea").prop("value")).toEqual("");
+  });
 });
