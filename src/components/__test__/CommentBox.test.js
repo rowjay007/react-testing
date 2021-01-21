42861f06221017b380e1fe040290cc91d7c7b70e
Rowland Adimoha <rowlives@gmail.com>
2021-01-21 08:56:06 +0100

    add stimulate() method to privde fake event in CommentBox

--- src/components/__test__/CommentBox.test.js
+++ src/components/__test__/CommentBox.test.js
@@ -14,3 +14,8 @@ it("has a text area and a button", () => {
   expect(wrapped.find("textarea").length).toEqual(1);
   expect(wrapped.find("button").length).toEqual(1);
 });
+it("has a text area that user can type in", () => {
+  wrapped
+    .find("textarea")
+    .stimulate("change", { target: { value: "new comment" } });
+});
