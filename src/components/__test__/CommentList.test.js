09956a947a23de7cba343d9cfbaef5214246c929
rowjay007 <rowlives@gmail.com>
2021-01-28 09:54:40 -0800

    create commentList test file

--- src/components/__test__/CommentList.test.js
+++ src/components/__test__/CommentList.test.js
@@ -3,3 +3,15 @@ import { mount } from "enzyme";
 import CommentList from "../../components/CommentList";
 
 import Root from "../../Roots";
+
+let wrapped;
+
+beforeEach(() => {
+  wrapped = mount(
+    <Root>
+      <CommentList />
+    </Root>
+  );
+});
+
+it("create one LI per comment", () => {});
