ac741cda16d1a1eb686b747a9e48527d2d16758c
rowjay007 <rowlives@gmail.com>
2021-02-04 08:08:38 +0100

    replace setTimeOut with moxios.wait function

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -24,10 +24,10 @@ it("can fetch a list of comments and display them", (done) => {
   );
 
   wrapped.find(".fetch-comments").simulate("click");
-  setTimeout(() => {
+  moxios.wait(() => {
     wrapped.update();
-    expect(wrapped.find("li").length).toEqual(3);
+    expect(wrapped.find("li").length).toEqual(2);
     done();
     wrapped.unmount();
-  }, 100);
+  });
 });
