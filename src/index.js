31cb64ac12d03532e18d14bcfeaffa0ffb63d6b6
rowjay007 <rowlives@gmail.com>
2021-02-04 08:18:20 +0100

    install react-router-dom

--- src/index.js
+++ src/index.js
@@ -1,11 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import App from "./components/App";
 import Roots from "./Roots";
 
 ReactDOM.render(
   <Roots>
-    <App />
+    <BrowserRouter>
+      <Route path="/" component={App} />
+      <App />
+    </BrowserRouter>
   </Roots>,
   document.querySelector("#root")
 );
