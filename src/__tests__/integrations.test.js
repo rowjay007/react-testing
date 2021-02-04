7648e3ff5130ad494d92b1886a859d417361b836
rowjay007 <rowlives@gmail.com>
2021-02-04 07:28:14 +0100

    create integration test folder

---
+++ src/__tests__/integrations.test.js
@@ -0,0 +1,6 @@
+import React from "react";
+import { mount } from "enzyme";
+import Root from "../Roots";
+import App from "../components/App";
+
+it("can fetch a list of comments and display them", () => {});
