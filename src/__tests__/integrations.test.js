4b671720d2857aed52ed15ba9e5d9f65d8412e8d
rowjay007 <rowlives@gmail.com>
2021-02-04 07:36:22 +0100

    simulate click button

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -1,6 +1,16 @@
 import React from "react";
 import { mount } from "enzyme";
-import Root from "../Roots";
 import App from "../components/App";
+import Roots from "../Roots";
 
-it("can fetch a list of comments and display them", () => {});
+it("can fetch a list of comments and display them", () => {
+  const wrapped = mount(
+    <Roots>
+      <App />
+    </Roots>
+  );
+
+  wrapped.find(".fetch-comments").simulate("click");
+
+  expect(wrapped.find("li").length).toEqual(500);
+});
