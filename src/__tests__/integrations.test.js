9067691805d0aeafc1aad52f20148ae066f4d3ac
rowjay007 <rowlives@gmail.com>
2021-02-04 07:49:01 +0100

    install moxios suit in integration folder

--- src/__tests__/integrations.test.js
+++ src/__tests__/integrations.test.js
@@ -2,6 +2,19 @@ import React from "react";
 import { mount } from "enzyme";
 import App from "../components/App";
 import Roots from "../Roots";
+import moxios from "moxios";
+
+beforeEach(() => {
+  moxios.install();
+  moxios.stubRequest("http://jsonplaceholder.typicode.com/comments", {
+    status: 200,
+    response: [{ name: "Fetched #1" }, { name: "Fetched #2" }],
+  });
+});
+
+afterEach(() => {
+  moxios.uninstall();
+});
 
 it("can fetch a list of comments and display them", () => {
   const wrapped = mount(
@@ -12,5 +25,5 @@ it("can fetch a list of comments and display them", () => {
 
   wrapped.find(".fetch-comments").simulate("click");
 
-  expect(wrapped.find("li").length).toEqual(500);
+  expect(wrapped.find("li").length).toEqual(2);
 });
