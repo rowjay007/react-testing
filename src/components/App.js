3d8a1fc22b16ede234a27494bb13742fcab8a712
rowjay007 <rowlives@gmail.com>
2021-02-05 07:55:25 +0100

    change app component to class and render items on home

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,31 @@
-import React from "react";
-import { Route } from "react-router-dom";
+import React, { Component } from "react";
+import { Route, Link } from "react-router-dom";
 import CommentBox from "./CommentBox";
 import CommentList from "./CommentList";
 
-const App = () => {
-  return (
-    <div>
-      <Route path="/post" component={CommentBox} />
-      <Route path="/" exact component={CommentList} />
-    </div>
-  );
-};
+class App extends Component {
+  renderButton() {}
+  renderHeader() {
+    return (
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/post">Post a Comment</Link>
+        </li>
+        <li>{this.renderButton()}</li>
+      </ul>
+    );
+  }
+  render() {
+    return (
+      <div>
+        <Route path="/post" component={CommentBox} />
+        <Route path="/" exact component={CommentList} />
+      </div>
+    );
+  }
+}
 
 export default App;
