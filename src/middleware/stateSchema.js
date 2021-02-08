dc05fb2f3bc0df4197d78112400fd99b0ef14f8c
rowjay007 <rowlives@gmail.com>
2021-02-08 11:12:33 +0100

    implement tv4 in stateValidator middleware

---
+++ src/middleware/stateSchema.js
@@ -0,0 +1,22 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  $schema: "http://json-schema.org/draft-04/schema#",
+  type: "object",
+  properties: {
+    comments: {
+      type: "array",
+      items: [
+        {
+          type: "string",
+        },
+        {
+          type: "string",
+        },
+      ],
+    },
+    auth: {
+      type: "boolean",
+    },
+  },
+  required: ["comments", "auth"],
+};
--- src/middleware/stateValidator.js
+++ src/middleware/stateValidator.js
@@ -1,2 +1,10 @@
 /* eslint-disable import/no-anonymous-default-export */
-export default ({ dispatch }) => (next) => (action) => {};
+import tv4 from "tv4";
+import stateSchema from "./stateSchema";
+export default ({ dispatch, getState }) => (next) => (action) => {
+  next(action);
+
+  if (tv4.validate(getState(), stateSchema)) {
+    console.log("Invalid state schema detected");
+  }
+};
